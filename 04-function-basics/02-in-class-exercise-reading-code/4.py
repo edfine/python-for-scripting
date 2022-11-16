635f668257c455ae8e8e8f34c5defa0747f792ca
Ed Fine <edward.fin@gmail.com>
2022-11-16 10:18:11 -0800

    add reference to pythontutor.net

--- 04-function-basics/02-in-class-exercise-reading-code/4.py
+++ 04-function-basics/02-in-class-exercise-reading-code/4.py
@@ -13,6 +13,15 @@ def loops_in_loops():
 
 loops_in_loops()
 
+def loops_in_loops_comp():
+    total = 0
+    big = ((i,j,total) for i in range(10) for j in range(10))
+    for tup in big:
+        print(tup)
+
+loops_in_loops_comp()
 # If this result confuses you, simulate its execution by
 # manually keeping track of the values in the variables 
 # i, j and c as you step through the code line by line.
+# Alternately you can examine this code using pythontutor.net
+
